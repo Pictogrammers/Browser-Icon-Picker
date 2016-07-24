d5c9e77937a4119335752176d6823f8151c2d5b8
Quentin Stoeckel <chteuchteu@gmail.com>
2016-07-24 22:35:34 +0200

    Include MaterialDesignIcons stylesheets & fonts from CDN instead of shipping it, closes #2

--- prepare-release.py
+++ prepare-release.py
@@ -14,10 +14,7 @@ release_dir = 'release/'
 base_dir = os.path.dirname(os.path.realpath(__file__))
 flavours = ['Chrome', 'Firefox']
 files_generic = [
-    'data/browser-action/css/materialdesignicons.min.css',
-    'data/browser-action/css/materialdesignicons.min.css.map',
     'data/browser-action/css/style.css',
-    'data/browser-action/fonts/*',
     'data/browser-action/js/script.js',
     'data/browser-action/vendor/jquery-2.2.3.min.js',
     'data/browser-action/index.html',
