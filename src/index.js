6782c6ea0740c795229a7431156fb4974f67669b
Quentin Stoeckel <chteuchteu@gmail.com>
2017-12-26 18:08:00 +0100

    Use just released vue-icons-picker

--- src/index.js
+++ src/index.js
@@ -8,10 +8,37 @@ require('./css/style.scss');
 
 // Dependencies
 import Vue from 'vue';
-import App from './vue/App.vue';
+import IconsPicker from 'vue-icons-picker';
 
 // Init VueJs component
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(IconsPicker, {
+        props: {
+            config: {
+                name: 'MaterialDesignIcons',
+                classPrefix: 'mdi',
+                repoUrl: 'https://github.com/chteuchteu/MaterialDesignIcons-Picker',
+
+                icons: {
+                    main: 'mdi mdi-vector-square',
+                    loading: 'mdi mdi-cached',
+                    close: 'mdi mdi-close',
+                    openExternal: 'mdi mdi-open-in-new',
+                    random: 'mdi mdi-shuffle',
+                    randomColors: 'mdi mdi-invert-colors',
+                    svg: 'mdi mdi-xml',
+                    madeBy: 'mdi mdi-heart',
+                    gitHub: 'mdi mdi-github-circle'
+                },
+
+                openText: 'Open MaterialDesignIcons.com',
+                openUrl: 'https://materialdesignicons.com',
+                openIconText: 'Open {icon} in MaterialDesignIcons.com',
+                openIconUrl: 'https://materialdesignicons.com/icon/{icon}',
+
+                canCopySvg: true,
+            }
+        }
+    })
 });
