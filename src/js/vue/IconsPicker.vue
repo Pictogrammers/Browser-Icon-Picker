947616e6326debd9ad86e2e7062c98709756596f
Quentin Stoeckel <stoeckel.quentin@gmail.com>
2019-01-19 20:15:39 +0100

    Extension rewrite: add support for MDI light, "outline" toggle, dark theme

--- src/js/vue/IconsPicker.vue
+++ src/js/vue/IconsPicker.vue
@@ -3,26 +3,69 @@
         class="app"
         :class="{
             ['accent-color--'+accentColor]: true,
+            'dark-theme': darkTheme,
             'random-icon-colors': randomColors
         }"
     >
         <header class="header">
             <div class="title">
-                <h1><i class="mdi mdi-vector-square"></i> MaterialDesignIcons</h1>
-                <small class="version">v {{ version }}</small>
+                <h1>
+                    <a href="https://materialdesignicons.com" target="_blank">
+                        <i class="mdi mdi-vector-square"></i> MaterialDesignIcons
+                    </a>
+                </h1>
+                <overflow-menu
+                    ref="overflow-main"
+                    :on-open="() => this.openOverflowMenu = this.$refs['overflow-main']"
+                    :on-close="() => this.openOverflowMenu = null"
+                    attach-to="top"
+                >
+                    <div @click="setActiveIcon(getRandomIcon(), true)">
+                        <i class="mdi mdi-shuffle"></i>
+                        Random icon
+                    </div>
+                    <div @click="filters.flavour = filters.flavour === 'light' ? 'default' : 'light'">
+                        <i class="mdil mdil-account"></i>
+                        Standard / light
+
+                        <setting-switch :value="filters.flavour === 'light'" />
+                    </div>
+                    <div @click="filters.outline = filters.outline === 'outline' ? 'both' : 'outline'">
+                        <i class="mdi mdi-account-multiple-outline"></i>
+                        Outline only
+
+                        <setting-switch :value="filters.outline === 'outline'" />
+                    </div>
+                    <div @click="randomColors = !randomColors">
+                        <i class="mdi mdi-palette"></i>
+                        Random colors
+
+                        <setting-switch :value="randomColors" />
+                    </div>
+                    <div @click="darkTheme = !darkTheme">
+                        <i class="mdi mdi-theme-light-dark"></i>
+                        Dark theme
+
+                        <setting-switch :value="darkTheme" />
+                    </div>
+                    <div class="overflow-footer">
+                        MDI v{{ version && version.default }} / MDI light v{{ version && version.light }}<br />
+                        <a href="https://github.com/chteuchteu/MaterialDesignIcons-Picker">Open on GitHub</a>
+                    </div>
+                </overflow-menu>
             </div>
-            <div class="filter-wrap">
+            <div class="search-wrap">
                 <input
-                    class="filter" type="text"
+                    class="search" type="text"
                     placeholder="Search..." autofocus
-                    v-model="filter"
+                    v-model="search"
                 />
                 <i
-                    class="filter-clear mdi mdi-close"
+                    class="search-clear mdi mdi-close"
                     :class="{
-                        hidden: filter.length === 0
+                        hidden: search.length === 0
                     }"
-                    @click="filter = ''"
+                    @click="search = ''"
                 ></i>
             </div>
         </header>
@@ -39,122 +82,73 @@
             >
                 <i
                     v-for="icon in filteredIcons"
+                    :key="icon.id"
                     class="mdi"
                     :class="{
-                        ['mdi-'+icon.name]: true,
+                        [icon.class]: true,
                         active: isIconActive && activeIcon === icon
                     }"
                     @click.stop="setActiveIcon(icon)"
                     :title="icon.name"
-                ></i>
+                />
+
+                <div class="properties-spacer"></div>
             </div>
 
             <transition name="properties">
                 <div class="icon-properties" v-show="isIconActive">
                     <i
                         class="icon-icon"
-                        :class="activeIcon && 'mdi mdi-'+activeIcon.name"
+                        :class="activeIcon && activeIcon.class"
                         @click="changeAccentColor"
                     ></i>
 
                     <div class="properties">
-                        <span class="icon-name">{{ activeIcon && activeIcon.name }}</span>
-                        <input
-                            class="icon-class"
-                            placeholder="Class"
-                            :value="'mdi mdi-' + (activeIcon && activeIcon.name)"
-                            disabled
-                        />
-                        <p class="icon-more">
+                        <overflow-menu
+                            ref="overflow-properties"
+                            :on-open="() => onPropertiesOverflowMenuOpened() || (this.openOverflowMenu = this.$refs['overflow-properties'])"
+                            :on-close="() => this.openOverflowMenu = null"
+                            attach-to="bottom"
+                        >
+                            <a
+                                v-show="isIconActive && !activeIcon.styles.includes('light')"
+                                :href="isIconActive && 'https://materialdesignicons.com/icon/{icon}'.replace('{icon}', activeIcon.name)"
+                                target="_blank"
+                            >
+                                <i class="mdi mdi-open-in-new"></i>
+                                {{ isIconActive && 'Open {icon}'.replace('{icon}', activeIcon.name) }}
+                            </a>
+                            <div @click="copySvg">
+                                <i class="mdi mdi-xml"></i>
+                                Copy SVG
+                            </div>
+                            <div @click="downloadSvg">
+                                <i class="mdi mdi-download"></i>
+                                Download SVG
+                            </div>
+                        </overflow-menu>
+
+                        <div class="icon-name">{{ activeIcon && activeIcon.name }}</div>
+                        <div class="icon-class">{{ activeIcon && activeIcon.class }}</div>
+                        <div class="icon-more">
                             <span class="icon-codepoint">{{ activeIcon && activeIcon.codepoint }}</span>
                             <span
-                                class="icon-version-wrap"
                                 v-show="activeIcon && activeIcon.version"
                             > &bullet; v<span class="icon-version">{{ activeIcon && activeIcon.version }}</span></span>
                             <span
-                                class="icon-author-wrap"
                                 v-show="activeIcon && activeIcon.author"
                             > &bullet; by <span class="icon-author">{{ activeIcon && activeIcon.author }}</span></span>
-                        </p>
+                        </div>
                     </div>
                 </div>
             </transition>
         </div>
-
-        <footer>
-            <div class="left">
-                <a
-                    class="btn-sm-round"
-                    :href="isIconActive
-                        ? 'https://materialdesignicons.com/icon/{icon}'.replace('{icon}', activeIcon.name)
-                        : 'https://materialdesignicons.com'"
-                    target="_blank"
-                    :title="isIconActive
-                        ? 'Open {icon} in MaterialDesignIcons.com'.replace('{icon}', activeIcon.name)
-                        : 'Open MaterialDesignIcons.com'
-                    "
-                >
-                    <i class="mdi mdi-open-in-new"></i>
-                </a>
-                <a
-                    class="btn-sm-round"
-                    href="#"
-                    @click.prevent="setActiveIcon(getRandomIcon(), true)"
-                    title="Random icon"
-                >
-                    <i class="mdi mdi-shuffle"></i>
-                </a>
-                <a
-                    class="btn-sm-round"
-                    :class="{
-                        active: randomColors
-                    }"
-                    href="#"
-                    @click.prevent="randomColors = !randomColors"
-                    title="Random colors"
-                >
-                    <i class="mdi mdi-invert-colors"></i>
-                </a>
-                <a
-                    class="btn-sm-round"
-                    href="#"
-                    v-show="isIconActive"
-                    @click.prevent="copySvg"
-                    title="Copy SVG"
-                >
-                    <i class="mdi mdi-xml"></i>
-                </a>
-                <a
-                    class="btn-sm-round"
-                    href="#"
-                    v-show="isIconActive"
-                    @click.prevent="downloadSvg"
-                    title="Download SVG"
-                >
-                    <i class="mdi mdi-download"></i>
-                </a>
-            </div>
-            <div class="right">
-                <a
-                    class="btn-sm-round"
-                    href="https://www.s-quent.in"
-                    target="_blank"
-                    title="Made with <3 by Quentin S."
-                >
-                    <i class="mdi mdi-heart"></i>
-                </a>
-                <a
-                    class="btn-sm-round"
-                    href="https://github.com/chteuchteu/MaterialDesignIcons-Picker"
-                    target="_blank"
-                    title="GitHub"
-                >
-                    <i class="mdi mdi-github-circle"></i>
-                </a>
-            </div>
-            <div class="clearfix"></div>
-            <input ref="input-copy" style="display: none" />
-        </footer>
+        <input ref="input-copy" style="display: none" />
+        <div
+            v-show="openOverflowMenu !== null"
+            class="overflow-menu-overlay"
+            @click="openOverflowMenu.close()"
+        ></div>
     </div>
 </template>
 
@@ -162,15 +156,23 @@
     import { request } from '../lib/Request';
     import { randomInt } from '../lib/Math';
     import { computeOffset, outerHeight } from '../lib/jQuery';
+    import SettingSwitch from "./SettingSwitch.vue";
+    import OverflowMenu from "./OverflowMenu.vue";
 
-    const ACCENT_COLOR_KEY = 'color-accent';
-    const RANDOM_COLORS_KEY = 'random-colors';
+    const SETTINGS = {
+        ACCENT_COLOR: 'color-accent',
+        RANDOM_COLORS: 'random-colors',
+        FLAVOUR: 'flavour',
+        OUTLINE: 'outline',
+        DARK: 'dark',
+    };
+
     const COLORS = [
         'red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green',
         'light-green', 'lime', 'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'
     ];
 
-    const filterReplaceRegex = new RegExp('-', 'g');
+    const searchReplaceRegex = new RegExp('-', 'g');
 
     const getResourceUrl = (filename) => typeof(chrome) !== 'undefined' && chrome.extension !== undefined
         ? chrome.extension.getURL('dist/data/' + filename)
@@ -178,32 +180,44 @@
 
     export default {
         name: 'icons-picker',
+        components: {OverflowMenu, SettingSwitch},
         data: () => ({
             loading: true,
-            filter: '',
-            icons: [],
-            version: null,
-            svg: [],
+            darkTheme: JSON.parse(localStorage.getItem(SETTINGS.DARK)) === true,
+            search: '',
+            filters: {
+                flavour: localStorage.getItem(SETTINGS.FLAVOUR) || 'default', // "default" vs "light"
+                outline: localStorage.getItem(SETTINGS.OUTLINE) || 'both', // "both" vs "outline"
+            },
+            icons: {},
+            version: {},
+            openOverflowMenu: null,
 
-            accentColor: null,
-            randomColors: true,
+            accentColor: localStorage.getItem(SETTINGS.ACCENT_COLOR) || 'primary',
+            randomColors: JSON.parse(localStorage.getItem(SETTINGS.RANDOM_COLORS)) === true,
             activeIcon: null,
             isIconActive: false,
+
+            cachedSvgs: {},
         }),
         computed: {
             filteredIcons() {
-                const filterVal = this.filter
-                    .replace(filterReplaceRegex, ' ')
+                if (this.loading) {
+                    return;
+                }
+
+                const searchVal = this.search
+                    .replace(searchReplaceRegex, ' ')
                     .toLowerCase();
 
-                return this.icons.filter((icon) => icon.searchable.indexOf(filterVal) !== -1);
-            }
+                return this.icons[this.filters.flavour].filter(
+                    (icon) =>
+                        icon.searchable.indexOf(searchVal) !== -1
+                        && (this.filters.outline === 'both' || icon.styles.includes(this.filters.outline))
+                );
+            },
         },
         mounted() {
-            // Load accent color settings
-            this.accentColor = localStorage.getItem(ACCENT_COLOR_KEY) || 'primary';
-            this.randomColors = JSON.parse(localStorage.getItem(RANDOM_COLORS_KEY)) === true;
-
             // Load icons
             request(getResourceUrl('icons.min.json'))
                 .then(JSON.parse)
@@ -211,15 +225,6 @@
                     this.loading = false;
                     this.icons = response.icons;
                     this.version = response.version;
-                });
-
-            this.loading = false;
-
-            // Load svg
-            request(getResourceUrl('icons-svg.min.json'))
-                .then(JSON.parse)
-                .then((response) => {
-                    this.svg = response;
                 });
         },
         methods: {
@@ -232,13 +237,13 @@
 
                     if (ensureVisible) {
                         // Wait for the UI to be updated for calculations
-                        const iconElem = document.querySelector('.mdi-'+icon.name),
+                        const iconElem = document.querySelector('.mdi-'+icon.name) || document.querySelector('.mdil-'+icon.name),
                             iconsList = this.$refs['icons'];
 
                         const offset = computeOffset(iconElem).top - computeOffset(iconsList).top,
                             iconElemHeight = outerHeight(iconElem, true);
 
-                        // Take properties pannel height into account when it appears
+                        // Take properties panel height into account when it appears
                         // (it's not shown yet)
                         let iconsListHeight = iconsList.clientHeight;
                         if (!hasActiveIcon) {
@@ -258,7 +263,7 @@
                 }
             },
             getRandomIcon() {
-                return this.icons[randomInt(0, this.icons.length-1)];
+                return this.icons[this.filters.flavour][randomInt(0, this.icons[this.filters.flavour].length-1)];
             },
             changeAccentColor() {
                 const i = COLORS.indexOf(this.accentColor)+1;
@@ -269,8 +274,13 @@
                     return;
                 }
 
-                const name = this.activeIcon.name,
-                    svg = this.svg[name],
+                let id = this.activeIcon.id;
+
+                if (Object.keys(this.cachedSvgs).indexOf(id) === -1) {
+                    return;
+                }
+
+                const svg = this.cachedSvgs[id],
                     input = this.$refs['input-copy'];
 
                 input.value = svg;
@@ -281,32 +291,67 @@
                     event.preventDefault();
                 };
                 document.execCommand("Copy", false, null);
+
+                // Close overflow menu once done
+                this.openOverflowMenu.close();
             },
             downloadSvg() {
                 if (this.activeIcon === null) {
                     return;
                 }
 
-                const name = this.activeIcon.name;
+                let id = this.activeIcon.id;
+
+                if (Object.keys(this.cachedSvgs).indexOf(id) === -1) {
+                    return;
+                }
+
                 // Add namespace to <svg tag
-                const svg = this.svg[name].replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ');
+                const svg = this.cachedSvgs[id].replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ');
 
                 const blob = new Blob([svg], {type: "text/plain"});
                 const url = URL.createObjectURL(blob);
 
                 chrome.downloads.download({
                     url: url,
-                    filename: name+'.svg',
+                    filename: this.activeIcon.name+'.svg',
                 });
+            },
+            onPropertiesOverflowMenuOpened() {
+                // Pre-fetch SVG
+                let id = this.activeIcon.id;
+
+                if (Object.keys(this.cachedSvgs).indexOf(id) !== -1) {
+                    return;
+                }
+
+                request(getResourceUrl(`svg/${id}.svg`))
+                    .then((response) => {
+                        this.cachedSvgs[id] = response;
+                    });
             },
         },
         watch: {
             accentColor() {
-                localStorage.setItem(ACCENT_COLOR_KEY, this.accentColor);
+                localStorage.setItem(SETTINGS.ACCENT_COLOR, this.accentColor);
             },
             randomColors() {
-                localStorage.setItem(RANDOM_COLORS_KEY, this.randomColors.toString());
+                localStorage.setItem(SETTINGS.RANDOM_COLORS, this.randomColors.toString());
+            },
+            darkTheme() {
+                localStorage.setItem(SETTINGS.DARK, this.darkTheme.toString());
+            },
+            'filters.flavour'() {
+                // When setting "flavour" to "light", force the "outline" one to "outline"
+                if (this.filters.flavour === 'light') {
+                    this.filters.outline = 'outline';
+                }
+
+                localStorage.setItem(SETTINGS.FLAVOUR, this.filters.flavour);
+            },
+            'filters.outline'() {
+                localStorage.setItem(SETTINGS.OUTLINE, this.filters.outline);
             }
-        }
+        },
     };
 </script>
---
+++ src/js/vue/SettingSwitch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        class="switch"
+        :class="{active: value}"
+    >
+        <div class="handle"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'setting-switch',
+        props: {
+            value: {
+                type: Boolean,
+                required: true,
+            },
+            onChange: {
+                type: Function,
+                required: false,
+            },
+        },
+    }
+</script>
