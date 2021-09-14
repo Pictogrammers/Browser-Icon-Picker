724cd03108b9051ac11dcc529834e41c00c24aae
Quentin Stoeckel <stoeckel.quentin@gmail.com>
2021-09-14 10:18:06 +0200

    upstream.js, pull-icons.js: codestyle updates

--- tools/lib/upstream.js
+++ tools/lib/upstream.js
@@ -8,110 +8,110 @@ const path = require('path');
 const yauzl = require('yauzl');
 
 const repo = {
-    default: 'MaterialDesign',
-    light: 'MaterialDesignLight',
+  default: 'MaterialDesign',
+  light: 'MaterialDesignLight',
 };
 const package = {
-    default: '@mdi/svg',
-    light: '@mdi/light-svg',
+  default: '@mdi/svg',
+  light: '@mdi/light-svg',
 }
 
 /**
  * Hits the specified URL, and returns the JSON-decoded response.
  */
 const getJson = (url) => {
-    return new Promise((resolve, reject) => {
-        request({url, json: true}, (error, response, body) => {
-            if (error) reject(error);
-            if (response.statusCode !== 200) {
-                reject('Invalid status code <' + response.statusCode + '> from URI ' + url);
-            }
+  return new Promise((resolve, reject) => {
+    request({url, json: true}, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode !== 200) {
+        reject('Invalid status code <' + response.statusCode + '> from URI ' + url);
+      }
 
-            resolve(body);
-        });
+      resolve(body);
     });
+  });
 };
 
 /**
  * Returns that latest known version for the specified flavour.
  */
 const getLatestVersion = async (flavour = 'default') => {
-    const packageJsonUrl = `https://raw.githubusercontent.com/Templarian/${repo[flavour]}-SVG/master/package.json`;
+  const packageJsonUrl = `https://raw.githubusercontent.com/Templarian/${repo[flavour]}-SVG/master/package.json`;
 
-    const body = await getJson(packageJsonUrl);
-    return body.version;
+  const body = await getJson(packageJsonUrl);
+  return body.version;
 };
 
 /**
  * Downloads the meta.json file for the specified version and flavour.
  */
 const getMeta = async (version, flavour = 'default') => {
-    const url = `https://cdn.jsdelivr.net/npm/${package[flavour]}@${version}/meta.json`;
-    return await getJson(url);
+  const url = `https://cdn.jsdelivr.net/npm/${package[flavour]}@${version}/meta.json`;
+  return await getJson(url);
 };
 
-const download = async(url, destPath) => {
-    return new Promise((resolve, reject) => {
-        request(url)
-            .on('error', () => reject())
-            .on('response', (res) => {
-                if (res.statusCode !== 200) {
-                    reject(res.statusCode);
-                    return;
-                }
+const download = async (url, destPath) => {
+  return new Promise((resolve, reject) => {
+    request(url)
+      .on('error', () => reject())
+      .on('response', (res) => {
+        if (res.statusCode !== 200) {
+          reject(res.statusCode);
+          return;
+        }
 
-                res.pipe(fs.createWriteStream(destPath));
-            })
-            .on('end', () => {
-                setTimeout(() => {
-                    resolve();
-                }, 500);
-            })
-    });
+        res.pipe(fs.createWriteStream(destPath));
+      })
+      .on('end', () => {
+        setTimeout(() => {
+          resolve();
+        }, 500);
+      })
+  });
 };
 
 /**
  * Downloads the SVG ZIP file for the specified flavour in the specified path.
  */
-const downloadSvgZip = async(destPath, flavour, version) => {
-    return download(`https://github.com/Templarian/${repo[flavour]}-SVG/archive/v${version}.zip`, destPath);
+const downloadSvgZip = async (destPath, flavour, version) => {
+  return download(`https://github.com/Templarian/${repo[flavour]}-SVG/archive/v${version}.zip`, destPath);
 };
 
-const downloadWebfontZip = async(destPath, flavour, version) => {
-    return download(`https://github.com/Templarian/${repo[flavour]}-Webfont/archive/v${version}.zip`, destPath);
+const downloadWebfontZip = async (destPath, flavour, version) => {
+  return download(`https://github.com/Templarian/${repo[flavour]}-Webfont/archive/v${version}.zip`, destPath);
 };
 
-const extractZip = async(zipPath, destDir, filePattern) => {
-    if (typeof filePattern === 'string') {
-        filePattern = new RegExp(filePattern);
-    }
+const extractZip = async (zipPath, destDir, filePattern) => {
+  if (typeof filePattern === 'string') {
+    filePattern = new RegExp(filePattern);
+  }
 
-    // Extract ZIP
-    return new Promise((resolve, reject) => {
-        yauzl.open(zipPath, (err, zipfile) => {
-            if (err) throw err;
+  // Extract ZIP
+  return new Promise((resolve, reject) => {
+    yauzl.open(zipPath, (err, zipfile) => {
+      if (err) throw err;
 
-            zipfile
-                .on('entry', (entry) => {
-                    if (filePattern.test(entry.fileName)) {
-                        zipfile.openReadStream(entry, function(err, readStream) {
-                            if (err) throw err;
+      zipfile
+        .on('entry', (entry) => {
+          if (filePattern.test(entry.fileName)) {
+            zipfile.openReadStream(entry, function (err, readStream) {
+              if (err) throw err;
 
-                            readStream.pipe(fs.createWriteStream(`${destDir}/${path.basename(entry.fileName)}`));
-                        });
-                    }
-                })
-                .on('close', () => {
-                    resolve();
-                });
+              readStream.pipe(fs.createWriteStream(`${destDir}/${path.basename(entry.fileName)}`));
+            });
+          }
+        })
+        .on('close', () => {
+          resolve();
         });
     });
+  });
 };
 
 module.exports = {
-    getLatestVersion,
-    getMeta,
-    downloadSvgZip,
-    downloadWebfontZip,
-    extractZip,
+  getLatestVersion,
+  getMeta,
+  downloadSvgZip,
+  downloadWebfontZip,
+  extractZip,
 };
--- tools/pull-icons.js
+++ tools/pull-icons.js
@@ -16,157 +16,157 @@ const workspace = path.resolve(__dirname, '../temp');
 const dist = path.resolve(__dirname, '../public');
 
 const readPackageJson = () => {
-    return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'));
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'));
 };
 
 const pullIcons = async () => {
-    log(chalk.blue.bold('MaterialDesignIcons icons parser🔍'));
+  log(chalk.blue.bold('MaterialDesignIcons icons parser🔍'));
 
-    // Check requested version
-    const packageJson = readPackageJson();
-    const versions = packageJson['materialdesignicons-picker'].version;
-    log(`Pulling versions default=${versions.default}, light=${versions.light}`);
+  // Check requested version
+  const packageJson = readPackageJson();
+  const versions = packageJson['materialdesignicons-picker'].version;
+  log(`Pulling versions default=${versions.default}, light=${versions.light}`);
 
-    // Create structure: temp directory & dist dir
-    const dirs = [
-        workspace,
-        dist + '/css/',
-        dist + '/fonts/',
-        dist + '/data/',
-        dist + '/data/svg/',
+  // Create structure: temp directory & dist dir
+  const dirs = [
+    workspace,
+    dist + '/css/',
+    dist + '/fonts/',
+    dist + '/data/',
+    dist + '/data/svg/',
+  ];
+  for (let directory of dirs) {
+    // Remove it if exists
+    if (fs.existsSync(directory)) {
+      rimraf.sync(directory);
+    }
+
+    // Re-create it
+    fs.mkdirSync(directory);
+  }
+
+  let data = {
+    icons: {
+      default: [],
+      light: [],
+    },
+    version: {
+      default: [],
+      light: [],
+    },
+  };
+
+  for (let flavour of ['default', 'light']) {
+    log(chalk.blue(`Handling flavour "${flavour}" 🔍`));
+
+    // Get requested version
+    const version = data.version[flavour] = versions[flavour];
+    log(`Requested version: ${chalk.underline(version)} ✔️`);
+
+    // Download CSS file (in dist/css/), woff2 webfont (in dist/fonts/)
+    const escapedVersion = version.replace('.', `\\.`);
+    const webfontZipPath = `${workspace}/${flavour}-webfont.zip`;
+    const webfontExtractedZipPath = `${workspace}/${flavour}-webfont`;
+    fs.mkdirSync(webfontExtractedZipPath);
+    try {
+      await upstream.downloadWebfontZip(webfontZipPath, flavour, version);
+    } catch (e) {
+      log(chalk.red('Failed.'));
+      return;
+    }
+    log(`Downloaded webfont ZIP ✔️`);
+    try {
+      await upstream.extractZip(webfontZipPath, webfontExtractedZipPath, `^MaterialDesign(Light)?-Webfont-${escapedVersion}\/(css\/materialdesignicons(-light)?\.min\.css|fonts\/materialdesignicons(-light)?-webfont\.woff2?)$`);
+    } catch (e) {
+      log(chalk.red('Failed.'));
+      return;
+    }
+    log(`Extracted webfont ZIP ✔️`);
+    // Copy files
+    const webfontZipFiles = [
+      `css/materialdesignicons${flavour === 'light' ? '-light' : ''}.min.css`,
+      `fonts/materialdesignicons${flavour === 'light' ? '-light' : ''}-webfont.woff`,
+      `fonts/materialdesignicons${flavour === 'light' ? '-light' : ''}-webfont.woff2`,
     ];
-    for (let directory of dirs) {
-        // Remove it if exists
-        if (fs.existsSync(directory)) {
-            rimraf.sync(directory);
-        }
+    for (const file of webfontZipFiles) {
+      const filename = file.split('/')[1]; // css/file.ext -> file.ext
+      fs.copyFileSync(`${webfontExtractedZipPath}/${filename}`, `${dist}/${file}`);
+    }
+    fs.copyFileSync(`${webfontExtractedZipPath}/materialdesignicons${flavour === 'light' ? '-light' : ''}.min.css`, `${dist}/css/materialdesignicons${flavour === 'light' ? '-light' : ''}.min.css`);
+    fs.copyFileSync(`${webfontExtractedZipPath}/materialdesignicons${flavour === 'light' ? '-light' : ''}-webfont.woff2`, `${dist}/fonts/materialdesignicons${flavour === 'light' ? '-light' : ''}-webfont.woff2`);
 
-        // Re-create it
-        fs.mkdirSync(directory);
+    // Download SVG files
+    const svgZipPath = `${workspace}/${flavour}-svg.zip`;
+    const svgExtractedZipPath = `${workspace}/${flavour}-svg`;
+    fs.mkdirSync(svgExtractedZipPath);
+    await upstream.downloadSvgZip(svgZipPath, flavour, version);
+    log(`Downloaded SVG ZIP ✔️`);
+    await upstream.extractZip(svgZipPath, svgExtractedZipPath, `^MaterialDesign(Light)?-SVG-${escapedVersion}\/svg\/.+$`);
+    log(`Extracted SVG ZIP ✔️`);
+
+    const iconsMetas = await upstream.getMeta(version, flavour);
+    log(`Downloaded meta file ✔️`);
+
+    for (let icon of iconsMetas) {
+      // Get svg, fine-tune it
+      let svg = fs.readFileSync(`${svgExtractedZipPath}/${icon.name}.svg`, 'utf-8');
+
+      // Remove xml header
+      svg = svg.replace('<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">', '');
+      // Remove xmlns & xmlns:xlink & version attributes
+      svg = svg.replace(' xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"', '');
+      // Remove id="{icon_name}" attribute
+      svg = svg.replace(` id=\"mdi-${icon.name}\"`, '');
+
+      fs.writeFileSync(`${dist}/data/svg/${icon.id}.svg`, svg);
+
+      // Compute "searchable" attribute
+      let searchable = '';
+      for (let part of [icon.name, ...icon.aliases, ...icon.tags]) {
+        searchable += part
+          .split('/').join(' ') // remove "/"
+          .split('-').join(' ') // remove "-"
+          .split('  ').join(' ') // replace multiple spaces
+          .toLowerCase()
+      }
+      searchable = Array.from(new Set(searchable.split(' '))).join(' ');
+
+      data.icons[flavour].push({
+        id: icon.id,
+        name: icon.name,
+        author: icon.author,
+        codepoint: icon.codepoint,
+        searchable: searchable,
+        version: icon.version,
+        styles: [
+          flavour, // 'light' or 'default'
+          icon.name.includes('outline') || flavour === 'light' ? 'outline' : 'filled', // 'outline' or 'filled'
+        ],
+        class: flavour === 'default'
+          ? 'mdi mdi-' + icon.name
+          : 'mdil mdil-' + icon.name,
+      });
     }
 
-    let data = {
-        icons: {
-            default: [],
-            light: [],
-        },
-        version: {
-            default: [],
-            light: [],
-        },
-    };
+    // Sort our icons list
+    data.icons[flavour].sort((a, b) => a.name.localeCompare(b.name));
 
-    for (let flavour of ['default', 'light']) {
-        log(chalk.blue(`Handling flavour "${flavour}" 🔍`));
+    log('');
+  }
 
-        // Get requested version
-        const version = data.version[flavour] = versions[flavour];
-        log(`Requested version: ${chalk.underline(version)} ✔️`);
+  // Dump to file
+  log('Writing JSON files...');
+  fs.writeFileSync(`${dist}/data/icons.json`, JSON.stringify(data, null, 4));
+  fs.writeFileSync(`${dist}/data/icons.min.json`, JSON.stringify(data));
+  log(chalk.green(`Done! ✔`));
 
-        // Download CSS file (in dist/css/), woff2 webfont (in dist/fonts/)
-        const escapedVersion = version.replace('.', `\\.`);
-        const webfontZipPath = `${workspace}/${flavour}-webfont.zip`;
-        const webfontExtractedZipPath = `${workspace}/${flavour}-webfont`;
-        fs.mkdirSync(webfontExtractedZipPath);
-        try {
-            await upstream.downloadWebfontZip(webfontZipPath, flavour, version);
-        } catch (e) {
-            log(chalk.red('Failed.'));
-            return;
-        }
-        log(`Downloaded webfont ZIP ✔️`);
-        try {
-            await upstream.extractZip(webfontZipPath, webfontExtractedZipPath, `^MaterialDesign(Light)?-Webfont-${escapedVersion}\/(css\/materialdesignicons(-light)?\.min\.css|fonts\/materialdesignicons(-light)?-webfont\.woff2?)$`);
-        } catch (e) {
-            log(chalk.red('Failed.'));
-            return;
-        }
-        log(`Extracted webfont ZIP ✔️`);
-        // Copy files
-        const webfontZipFiles = [
-            `css/materialdesignicons${flavour === 'light' ? '-light': ''}.min.css`,
-            `fonts/materialdesignicons${flavour === 'light' ? '-light': ''}-webfont.woff`,
-            `fonts/materialdesignicons${flavour === 'light' ? '-light': ''}-webfont.woff2`,
-        ];
-        for (const file of webfontZipFiles) {
-            const filename = file.split('/')[1]; // css/file.ext -> file.ext
-            fs.copyFileSync(`${webfontExtractedZipPath}/${filename}`, `${dist}/${file}`);
-        }
-        fs.copyFileSync(`${webfontExtractedZipPath}/materialdesignicons${flavour === 'light' ? '-light': ''}.min.css`, `${dist}/css/materialdesignicons${flavour === 'light' ? '-light': ''}.min.css`);
-        fs.copyFileSync(`${webfontExtractedZipPath}/materialdesignicons${flavour === 'light' ? '-light': ''}-webfont.woff2`, `${dist}/fonts/materialdesignicons${flavour === 'light' ? '-light': ''}-webfont.woff2`);
+  // Clean
+  rimraf.sync(workspace);
 
-        // Download SVG files
-        const svgZipPath = `${workspace}/${flavour}-svg.zip`;
-        const svgExtractedZipPath = `${workspace}/${flavour}-svg`;
-        fs.mkdirSync(svgExtractedZipPath);
-        await upstream.downloadSvgZip(svgZipPath, flavour, version);
-        log(`Downloaded SVG ZIP ✔️`);
-        await upstream.extractZip(svgZipPath, svgExtractedZipPath, `^MaterialDesign(Light)?-SVG-${escapedVersion}\/svg\/.+$`);
-        log(`Extracted SVG ZIP ✔️`);
-
-        const iconsMetas = await upstream.getMeta(version, flavour);
-        log(`Downloaded meta file ✔️`);
-
-        for (let icon of iconsMetas) {
-            // Get svg, fine-tune it
-            let svg = fs.readFileSync(`${svgExtractedZipPath}/${icon.name}.svg`, 'utf-8');
-
-            // Remove xml header
-            svg = svg.replace('<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">', '');
-            // Remove xmlns & xmlns:xlink & version attributes
-            svg = svg.replace(' xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"', '');
-            // Remove id="{icon_name}" attribute
-            svg = svg.replace(` id=\"mdi-${icon.name}\"`, '');
-
-            fs.writeFileSync(`${dist}/data/svg/${icon.id}.svg`, svg);
-
-            // Compute "searchable" attribute
-            let searchable = '';
-            for (let part of [icon.name, ...icon.aliases, ...icon.tags]) {
-                searchable += part
-                    .split('/').join(' ') // remove "/"
-                    .split('-').join(' ') // remove "-"
-                    .split('  ').join(' ') // replace multiple spaces
-                    .toLowerCase()
-            }
-            searchable = Array.from(new Set(searchable.split(' '))).join(' ');
-
-            data.icons[flavour].push({
-                id: icon.id,
-                name: icon.name,
-                author: icon.author,
-                codepoint: icon.codepoint,
-                searchable: searchable,
-                version: icon.version,
-                styles: [
-                    flavour, // 'light' or 'default'
-                    icon.name.includes('outline') || flavour === 'light' ? 'outline' : 'filled', // 'outline' or 'filled'
-                ],
-                class: flavour === 'default'
-                    ? 'mdi mdi-'+icon.name
-                    : 'mdil mdil-'+icon.name,
-            });
-        }
-
-        // Sort our icons list
-        data.icons[flavour].sort((a, b) => a.name.localeCompare(b.name));
-
-        log('');
-    }
-
-    // Dump to file
-    log('Writing JSON files...');
-    fs.writeFileSync(`${dist}/data/icons.json`, JSON.stringify(data, null, 4));
-    fs.writeFileSync(`${dist}/data/icons.min.json`, JSON.stringify(data));
-    log(chalk.green(`Done! ✔`));
-
-    // Clean
-    rimraf.sync(workspace);
-
-    // Report
-    log('Download finished!');
-    log(`Default flavour: ${versions.default}`);
-    log(`Light flavour: ${versions.light}`);
+  // Report
+  log('Download finished!');
+  log(`Default flavour: ${versions.default}`);
+  log(`Light flavour: ${versions.light}`);
 };
 
 pullIcons();
