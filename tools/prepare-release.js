7ab6f954eebbe97b9fe15d98642e878c934b9e35
Quentin Stoeckel <stoeckel.quentin@gmail.com>
2021-09-14 11:45:10 +0200

    prepare-release.js: codestyle updates

--- tools/prepare-release.js
+++ tools/prepare-release.js
@@ -15,79 +15,79 @@ const glob = require('glob');
 const chalk = require('chalk');
 const yazl = require('yazl');
 const stream = require('stream');
-const { execSync } = require('child_process');
+const {execSync} = require('child_process');
 
 const root = path.resolve(__dirname, '..'),
-    releaseDir = path.resolve(root, 'release');
+  releaseDir = path.resolve(root, 'release');
 
 const BUILDS = {
-    FIREFOX: 'Firefox',
-    CHROME: 'Chrome',
+  FIREFOX: 'Firefox',
+  CHROME: 'Chrome',
 };
 
 const FILES = [
-    'dist/index.html',
-    'dist/css/*',
-    'dist/fonts/*',
-    'dist/img/*',
-    'dist/js/*',
-    'dist/data/svg/*',
+  'dist/index.html',
+  'dist/css/*',
+  'dist/fonts/*',
+  'dist/img/*',
+  'dist/js/*',
+  'dist/data/svg/*',
 ];
 
 const readManifest = () => {
-    return JSON.parse(fs.readFileSync(root+'/manifest.json', 'utf8'));
+  return JSON.parse(fs.readFileSync(root + '/manifest.json', 'utf8'));
 };
 
 const readPackageJson = () => {
-    return JSON.parse(fs.readFileSync(root+'/package.json', 'utf8'));
+  return JSON.parse(fs.readFileSync(root + '/package.json', 'utf8'));
 };
 
 const prepareRelease = (build) => {
-    console.log(chalk.blue(`Preparing release for flavour "${build}"`));
+  console.log(chalk.blue(`Preparing release for flavour "${build}"`));
 
-    // Open manifest and read version name
-    const manifest = readManifest();
-    const version = manifest.version;
+  // Open manifest and read version name
+  const manifest = readManifest();
+  const version = manifest.version;
 
-    // Create release dir if it does not exist
-    if (!fs.existsSync(releaseDir)) {
-        fs.mkdirSync(releaseDir);
+  // Create release dir if it does not exist
+  if (!fs.existsSync(releaseDir)) {
+    fs.mkdirSync(releaseDir);
+  }
+
+  // Create final package
+  const zip = new yazl.ZipFile();
+
+  // Copy files
+  for (let pattern of FILES) {
+    for (let file of glob.sync(pattern)) {
+      const filePath = path.resolve(root, file);
+
+      zip.addFile(filePath, file);
     }
+  }
 
-    // Create final package
-    const zip = new yazl.ZipFile();
+  // Add the manifest
+  // Chrome build: rewrite manifest to remove Firefox's specific nodes
+  if (build === BUILDS.CHROME) {
+    delete manifest.applications;
+  }
 
-    // Copy files
-    for (let pattern of FILES) {
-        for (let file of glob.sync(pattern)) {
-            const filePath = path.resolve(root, file);
+  const manifestStream = new stream.Readable;
+  manifestStream.push(JSON.stringify(manifest, null, 4));
+  manifestStream.push(null);
+  zip.addReadStream(manifestStream, 'manifest.json');
+  zip.end();
 
-            zip.addFile(filePath, file);
-        }
-    }
-
-    // Add the manifest
-    // Chrome build: rewrite manifest to remove Firefox's specific nodes
-    if (build === BUILDS.CHROME) {
-        delete manifest.applications;
-    }
-
-    const manifestStream = new stream.Readable;
-    manifestStream.push(JSON.stringify(manifest, null, 4));
-    manifestStream.push(null);
-    zip.addReadStream(manifestStream, 'manifest.json');
-    zip.end();
-
-    // Write ZIP to disk
-    console.log('Writing ZIP file to disk...');
-    console.log('');
-    const zipExtension = build === BUILDS.FIREFOX ? 'xpi' : 'zip';
-    const zipName = `MaterialDesignIcons-Picker-${build}-${version}.${zipExtension}`;
-    zip.outputStream
-        .pipe(fs.createWriteStream(releaseDir + '/' + zipName))
-        .on('close', () => {
-            console.log(chalk.green(`Finished build "${build}" (${zipName}) ✔`));
-        });
+  // Write ZIP to disk
+  console.log('Writing ZIP file to disk...');
+  console.log('');
+  const zipExtension = build === BUILDS.FIREFOX ? 'xpi' : 'zip';
+  const zipName = `MaterialDesignIcons-Picker-${build}-${version}.${zipExtension}`;
+  zip.outputStream
+    .pipe(fs.createWriteStream(releaseDir + '/' + zipName))
+    .on('close', () => {
+      console.log(chalk.green(`Finished build "${build}" (${zipName}) ✔`));
+    });
 };
 
 /**
@@ -95,70 +95,70 @@ const prepareRelease = (build) => {
  * sources. This function generates a copy of this project, containing only the necessary files.
  */
 const prepareFirefoxReviewZip = () => {
-    console.log(chalk.blue(`Preparing Firefox review ZIP file`));
+  console.log(chalk.blue(`Preparing Firefox review ZIP file`));
 
-    const files = [
-        '*',
-        'dist/**/*',
-        'doc/*',
-        'src/**/*',
-        'tools/**/*',
-    ];
+  const files = [
+    '*',
+    'dist/**/*',
+    'doc/*',
+    'src/**/*',
+    'tools/**/*',
+  ];
 
-    const zip = new yazl.ZipFile();
+  const zip = new yazl.ZipFile();
 
-    // Copy files
-    for (let pattern of files) {
-        for (let file of glob.sync(pattern)) {
-            const filePath = path.resolve(root, file);
+  // Copy files
+  for (let pattern of files) {
+    for (let file of glob.sync(pattern)) {
+      const filePath = path.resolve(root, file);
 
-            // Check if it's really a file: '*' pattern matches directories too
-            const stat = fs.lstatSync(filePath);
-            if (stat.isFile()) {
-                zip.addFile(filePath, file);
-            }
-        }
+      // Check if it's really a file: '*' pattern matches directories too
+      const stat = fs.lstatSync(filePath);
+      if (stat.isFile()) {
+        zip.addFile(filePath, file);
+      }
     }
+  }
 
-    zip.end();
+  zip.end();
 
-    // Write ZIP to disk
-    console.log('Writing ZIP file to disk...');
-    console.log('');
-    const version = readManifest().version;
-    const zipName = `MaterialDesignIcons-Picker-Firefox-Review-${version}.zip`;
-    zip.outputStream
-        .pipe(fs.createWriteStream(releaseDir + '/' + zipName))
-        .on('close', () => {
-            console.log(chalk.green(`${zipName} is ready ✔`));
-        });
+  // Write ZIP to disk
+  console.log('Writing ZIP file to disk...');
+  console.log('');
+  const version = readManifest().version;
+  const zipName = `MaterialDesignIcons-Picker-Firefox-Review-${version}.zip`;
+  zip.outputStream
+    .pipe(fs.createWriteStream(releaseDir + '/' + zipName))
+    .on('close', () => {
+      console.log(chalk.green(`${zipName} is ready ✔`));
+    });
 };
 
 const updateVersionNumbers = (version) => {
-    console.log(chalk.blue(`Updating version numbers`));
+  console.log(chalk.blue(`Updating version numbers`));
 
-    if (version) {
-      console.log(`Using provided version ${version}`)
-    } else {
-      version = readManifest().version;
-      const parts = version.split('.');
-      parts[1]++; // v2.9.0 -> v2.10.0
-      parts[2] = '0'; // v2.9.1 -> v2.10.0
-      version = parts.join('.');
-    }
+  if (version) {
+    console.log(`Using provided version ${version}`)
+  } else {
+    version = readManifest().version;
+    const parts = version.split('.');
+    parts[1]++; // v2.9.0 -> v2.10.0
+    parts[2] = '0'; // v2.9.1 -> v2.10.0
+    version = parts.join('.');
+  }
 
-    // We could decode, update and re-encode JSON, but it would mess up with current formatting: use sed instead
-    for (let file of ['manifest.json', 'package.json']) {
-        console.log('Updating '+file);
-        execSync(`sed -i -e "s/\\"version\\": \\".*\\",/\\"version\\": \\"${version}\\",/" ./${file}`);
-    }
+  // We could decode, update and re-encode JSON, but it would mess up with current formatting: use sed instead
+  for (let file of ['manifest.json', 'package.json']) {
+    console.log('Updating ' + file);
+    execSync(`sed -i -e "s/\\"version\\": \\".*\\",/\\"version\\": \\"${version}\\",/" ./${file}`);
+  }
 };
 
 const createVersionBumpCommit = () => {
-    console.log(chalk.blue(`Creating version bump commit`));
-    const version = readManifest().version;
-    execSync(`git add manifest.json && git add package.json && git commit -m "v${version} version bump"`);
-    console.log(`Don't forget to push!`);
+  console.log(chalk.blue(`Creating version bump commit`));
+  const version = readManifest().version;
+  execSync(`git add manifest.json && git add package.json && git commit -m "v${version} version bump"`);
+  console.log(`Don't forget to push!`);
 };
 
 /* DO THE THING */
@@ -166,11 +166,11 @@ console.log(chalk.blue.bold('MaterialDesignIcons release bundler'));
 
 console.log(`Building project using webpack...`);
 try {
-    execSync('yarn run build');
+  execSync('yarn run build');
 } catch (error) {
-    console.log(chalk.blue.red('Build failed:'));
-    console.log(error);
-    return;
+  console.log(chalk.blue.red('Build failed:'));
+  console.log(error);
+  return;
 }
 console.log('');
 
@@ -180,7 +180,7 @@ updateVersionNumbers(nextVersion);
 console.log('');
 
 for (const build of Object.keys(BUILDS)) {
-    prepareRelease(BUILDS[build]);
+  prepareRelease(BUILDS[build]);
 }
 
 prepareFirefoxReviewZip();
